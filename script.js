216876f0ef543ef8836701c3847806dd310aca4c
emanrmz <37516737+emanrmz@users.noreply.github.com>
2018-04-04 19:14:30 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-var ball = document.getElementById("animation");
-var body = document.getElementsByTagName("body")[0];
-
 window.onload = function() {
-ball.className = "bounce";
-body.className = "backgroundSlide";
+
+let animationObject = document.getElementById("animationObject");
+let body = document.getElementsByTagName("body")[0];
+let animationEnd = document.getElementById("animationEnd");
+
+function createH1() {
+  let h1 = document.createElement("h1");
+  h1.className = "heroName";
+  h1.innerHTML = "Emmanuel<br>Ramirez";
+  body.insertBefore(h1, animationObject);
 }
 
+function removeFlex() {
+  body.style.justifyContent = "left";
+}
 
-// Test message for branch
+animationObject.className = "animationSequence";
+body.className = "backgroundSlide";
+animationEnd.addEventListener("animationend", createH1);
+
+} //END of Onload
